93936f01970bf3e8c0cf78dd16c422890027a2a8
ThanhTuan1607 <nguyentuan1235868@gmail.com>
2024-02-16 00:30:36 +0700

    finish

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URI
+    baseURL: 'https://ecommerce-be-omega.vercel.app/api'
   });
 
 // Add a request interceptor
